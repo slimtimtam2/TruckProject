9035b1a426e75cec01595ae57ae472a7b5eabc88
slimtimtam2 <taralaughlin769@gmail.com>
2021-08-23 21:08:01 -0400

    initial rest functions

---
+++ src/main/java/com/example/demo1/TruckController.java
@@ -0,0 +1,42 @@
+package com.example.demo1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TruckController {
+
+
+    private final TruckRepository repository;
+
+    TruckController(TruckRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("/trucks")
+    List<Truck> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("addTrucks")
+    Truck addTruck(@RequestBody Truck truck){
+        return repository.save(truck)
+    }
+
+    @GetMapping("/trucks/{id}")
+    Truck truckByLocationId(@PathVariable String id) {
+
+        return repository.findById(id)
+                .orElseThrow();
+    }
+
+    @GetMapping("/trucks/block/{block}")
+    List<Truck> truckByBlock(@PathVariable String block) {
+        List<Truck> blockList;
+        blockList = repository.findByBlock(block);
+       return blockList;
+    }
+}
